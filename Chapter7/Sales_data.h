9bdd9f642b119d276ed8db9f88cf1f3f7489c079
Weiyanyu <chinayeonon@gmail.com>
2017-04-04 22:15:31 +0800

    update

--- Chapter7/Sales_data.h
+++ Chapter7/Sales_data.h
@@ -10,12 +10,14 @@ class Sales_data{
 	friend std::istream &read(std::istream&, Sales_data&);
 	friend std::ostream &print(std::ostream&, const Sales_data&);
 public:
-	Sales_data() = default;
 	Sales_data(const std::string &s, unsigned n, double p):
 				bookNo(s),unit_sole(n),revenue(p*n) { }
-	Sales_data(const std::string &s):
-				bookNo(s) { }
-	Sales_data(std::istream&);
+	Sales_data():
+		Sales_data("",0,0) { }
+	Sales_data(std::string s):
+				Sales_data(s,0,0) { }
+	Sales_data(std::istream &is);
+			
 	std::string isbn() const { return bookNo; }
 	Sales_data &combine(const Sales_data &);
 private:
